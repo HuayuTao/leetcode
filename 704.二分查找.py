d96e7ba4cf8ed1236e953cb2ca32a0b20ce1a3ce
thy <77597843+huayu-tao@users.noreply.github.com>
2023-03-27 16:52:16 +0800

    Add files via upload

---
+++ 704.二分查找.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=704 lang=python3
+#
+# [704] 二分查找
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if(nums[mid]<target):
+                low=mid+1
+            elif(nums[mid]>target):
+                high=mid-1
+            else:
+                return mid  
+        return -1      
+# @lc code=end
+
