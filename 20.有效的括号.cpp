6d2e072261bbab0fd75dc57e89aee9fad7b0f0d8
thy <77597843+HuayuTao@users.noreply.github.com>
2023-04-07 16:31:27 +0800

    Add files via upload

---
+++ 20.有效的括号.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=cpp
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == '(')
+                st.push(')');
+            else if (s[i] == '[')
+                st.push(']');
+            else if (s[i] == '{')
+                st.push('}');
+            else if (st.empty() || st.top() != s[i])
+                return false;
+            else
+                st.pop();
+        }
+        return st.empty();
+    }
+};
+// @lc code=end
+
