c6bcd2b9461fb2344ad82e56b5e479e0f2bfc777
thy <77597843+HuayuTao@users.noreply.github.com>
2023-04-07 16:11:48 +0800

    Add files via upload

---
+++ 232.用栈实现队列.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue {
+    stack<int> stIn;
+    stack<int> stOut;
+
+public:
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        stIn.push(x);
+    }
+    
+    int pop() {
+        if (stOut.empty()){
+            while (!stIn.empty()){
+                stOut.push(stIn.top());
+                stIn.pop();
+            }
+        }
+        int result = stOut.top();
+        stOut.pop();
+        return result;
+    }
+    
+    int peek() {
+        int s = this -> pop();
+        stOut.push(s);
+        return s;
+    }
+    
+    bool empty() {
+        return stIn.empty() && stOut.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
